0e2d698c64a5dc6900db718e235cf6775b68c455
Jaime Rieiro <jaimeribeiro99@gmail.com>
2020-08-06 19:13:44 -0300

    :art: Making route work

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+interface RepositoryParams {
+    fullname: string;
+}
 
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    return (
+        <h1>
+            Repository:{params.fullname}
+        </h1>
+    );
 }
 export default Repository;
